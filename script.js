9b17d8f1ae69926bc8db30bb8ca0540e317e402e
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2025-08-19 13:23:41 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let hascat = false
+function pickupCat () {
+    hascat = true
+   document.getElementById("cat").remove()
+}
+let hasPatrol = false
+function pickupPatrol () {
+    hasPatrol = true
+   document.getElementById("Patrol").remove()
+}
+let hasSkull = false
+function pickupSkull () {
+    hasSkull = true
+   document.getElementById("Skull").remove()
+}
+function showinventory (){
+    const inventoryelement = document.getElementById ("Inventory")
+    let inventoryhtml = "<h2>Velkommen til Inventory</h2>"
+
+    if (hascat) {
+        inventoryhtml += `<div>
+            <img src="./assets/cat.jpg" />
+            <div>Katt</div>
+        </div>`
+    }
+
+    if (hasPatrol) {
+        inventoryhtml += `<div>
+            <img src="./assets/patrol.jpg" />
+            <div>Nissan Patrol</div>
+        </div>`
+    }
+
+    if (hasSkull) {
+        inventoryhtml += `<div>
+            <img src="./assets/skull.webp" />
+            <div>Team 8 (skull)</div>
+        </div>`
+    }
+
+    inventoryhtml += `<button onclick="closeinventory()">Lukk Inventory</button>`
+
+    inventoryelement.innerHTML = inventoryhtml
+}
+
+function closeinventory() {
+    const inventoryelement = document.getElementById ("Inventory")
+    inventoryelement.innerHTML = `<button onclick="showinventory()">Vis inventory</button>`
+}
